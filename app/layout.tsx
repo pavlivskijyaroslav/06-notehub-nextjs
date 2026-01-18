3c739273c7b7baef29af82b2cd34ead54756066a
yarik <aroslavpavlivskij@gmail.com>
2026-01-18 09:24:08 +0200

    add folder tanstack

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export const metadata: Metadata = {
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
-
 export default function RootLayout({
   children,
 }: Readonly<{
